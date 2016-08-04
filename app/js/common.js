8a9c092d9205bbae1e9b221888222e65dd13f9b6
TheDonKing <starspersona@gmail.com>
2016-08-04 18:26:12 +0300

    Debug safari

--- app/js/common.js
+++ app/js/common.js
@@ -165,7 +165,32 @@ $(window).on("mousemove",function(e) {
 	card.attr("style", "transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-webkit-transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-moz-transform: rotateY("+ax+"deg) rotateX("+ay+"deg)");
 });
 
+(function (d, w, c) {
+	(w[c] = w[c] || []).push(function() {
+		try {
+			w.yaCounter37668435 = new Ya.Metrika({
+				id:37668435,
+				clickmap:true,
+				trackLinks:true,
+				accurateTrackBounce:true,
+				webvisor:true
+			});
+		} catch(e) { }
+	});
 
+	var n = d.getElementsByTagName("script")[0],
+			s = d.createElement("script"),
+			f = function () { n.parentNode.insertBefore(s, n); };
+	s.type = "text/javascript";
+	s.async = true;
+	s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+	if (w.opera == "[object Opera]") {
+		d.addEventListener("DOMContentLoaded", f, false);
+	} else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+
+$("#map_canvas").load("test.php", { 'choices[]': ["Jon", "Susan"] } );
 
 
 
